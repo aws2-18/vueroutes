b60c1df7dfc37978e6492b4e1990d8e9b8933fb0
vagrant <vagrant@homestead>
2017-07-11 10:51:52 +0000

    test it

---
+++ resources/assets/js/routes.js
@@ -0,0 +1,23 @@
+import VueRouter from 'vue-router';
+
+let routes = [
+    {
+        path: '/',
+        component: require('./views/Home.vue')
+    },
+
+    {
+        path: '/about',
+        component: require('./views/Test.vue')
+    },
+
+    {
+        path: '/contact',
+        component: require('./views/Contact.vue')
+    }
+];
+
+export default new VueRouter({
+    routes,
+    linkActiveClass: 'is-active'
+});
